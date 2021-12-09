14bfc2d0d455307878711cfdfccd810e667eef20
David Sotelo <juandavid.sotelo.m@gmail.com>
2021-12-08 20:04:21 -0500

    Agregado logo al home

--- src/components/home/App.js
+++ src/components/home/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "../../Assets/LogoPNG.png";
 import "./App.css";
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Pagina en constricción</p>
+        <p>Pagina en construcción</p>
       </header>
     </div>
   );
