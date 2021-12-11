493ab350bad9e9a5aad585e354045eaa11e5a136
David Sotelo <juandavid.sotelo.m@gmail.com>
2021-12-11 14:50:20 -0500

    Mejora del footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,52 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
 import { BsFacebook, BsInstagram, BsYoutube } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import logo from "../Assets/Logo2.png";
 
 export const Footer = () => {
   return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col md="auto">Carrera 25 # 10-72 Barrio Obrero - Pasto</Col>
-      </Row>
-      <Row className="justify-content-md-center">
-        <Col md="auto">
-          <a href="https://www.facebook.com/Roninsatorimma/">
-            <BsFacebook />
-          </a>
-        </Col>
-        <Col md="auto">
-          <a href="https://www.instagram.com/roninsatorimma/">
-            <BsInstagram />
-          </a>
-        </Col>
-        <Col md="auto">
-          <a href="https://www.youtube.com/channel/UCQPDBalc7BAUIfUOKtrh48w">
-            <BsYoutube />
-          </a>
-        </Col>
-      </Row>
-    </Container>
+    <div className="bg-dark text-light">
+      <Container>
+        <Row className="pt-3 text-center ">
+          <Col sm className="mb-4">
+            <p>
+              <b> CONTACTANOS</b>
+              <br /> <br />
+              Diagonal 16D #6E-80 <br />
+              Villa Olímpica, Pasto <br />
+              Nariño - Colombia <br />
+              Celular: (+57) 301 252 4990 <br />
+              Correo:
+            </p>
+          </Col>
+          <Col sm className="mb-4">
+            <p>
+              <b>Satori MMA</b>
+            </p>
+            <Link to="/">
+              <img alt="Logo Satori" src={logo} width="150rm" height="auto" />
+            </Link>
+          </Col>
+          <Col sm>
+            <p className="mb-5">
+              <b>SIGUENOS EN:</b>
+            </p>
+            <a className="m-1" href="https://www.facebook.com/Roninsatorimma/">
+              <BsFacebook size="25%" />
+            </a>
+            <a className="m-1" href="https://www.instagram.com/roninsatorimma/">
+              <BsInstagram size="25%" />
+            </a>
+            <a
+              className="m-1"
+              href="https://www.youtube.com/channel/UCQPDBalc7BAUIfUOKtrh48w"
+            >
+              <BsYoutube size="25%" />
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
