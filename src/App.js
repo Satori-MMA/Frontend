04d47f182c8f5817b9d7aa213149c5b77759e49b
David Sotelo <juandavid.sotelo.m@gmail.com>
2021-12-12 16:37:04 -0500

    Ejemplo carga de datos de grahpql

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavigationBar } from "./components/NavigationBar";
 import { Footer } from "./components/Footer";
 import { About } from "./components/about/about";
+import { useQuery } from "@apollo/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ALL_USERS from "./graphql/users/ALL_USERS";
+import { Container, ListGroup, Spinner } from "react-bootstrap";
 function App() {
   return (
     <Router className="App">
@@ -24,13 +27,32 @@ function App() {
   );
 }
 
-
 const Home = () => {
+  const { data, loading } = useQuery(ALL_USERS);
   return (
     <div>
       <img src={logo} className="App-logo" alt="logo" />
-      <p>Pagina en construcción</p>
+      <p>Ejemplo fetching</p>
+      <Container>
+        {loading ? (
+          <center>
+            <Spinner animation="border" variant="danger" />
+          </center>
+        ) : (
+          <ListUsers users={data.users} />
+        )}
+      </Container>
     </div>
+  );
+};
+
+const ListUsers = ({ users }) => {
+  return (
+    <ListGroup>
+      {users.edges.map((row) => (
+        <ListGroup.Item key={row.node.id}> {row.node.email}</ListGroup.Item>
+      ))}
+    </ListGroup>
   );
 };
 const Cursos = () => {
