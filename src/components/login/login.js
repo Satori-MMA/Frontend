95bf07ca06c35ee5bce197121a6e72db456d2053
Oscar Chauza <ochauza@unicauca.edu.co>
2021-12-16 17:14:01 -0500

    Cambio de colores de login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -3,69 +3,114 @@ import { FaUserCircle } from "react-icons/fa";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { COLORS } from "../utilities/color";
+import LOGIN from "../../graphql/users/LOGIN";
+import { useMutation } from '@apollo/client';
+import { useState } from "react";
 
 export const Login = () => {
-  return (
-    <>
-      <Container>
-        <Row>
-          <Col lg={4} md={6} sm={12} className="pt m-auto shadow-sm rounded-lg">
-            <Form className="form-border">
-              <Row className="icon-user">
-                <FaUserCircle color={COLORS.carnelian} size={150} />
-              </Row>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <InputGroup>
-                  <InputGroup.Text>
-                    <FaUserCircle color={COLORS.carnelian} size={30} />
-                  </InputGroup.Text>
-                  <Form.Control
-                    type="email"
-                    placeholder="Ingrese su correo electronico"
-                  />
-                </InputGroup>
-              </Form.Group>
+    const [tokenAuth, { data, loading, error }] = useMutation(LOGIN);
+    const [datalogin, setDatalogin] = useState({ email: '', password: '' });
+    
+    if (loading) return 'Submitting...';
+    
+    if (error) return `Submission error! ${error.message}`;
+    
+    if(typeof(data) != "undefined"){
+        if(data.tokenAuth.success){
+            console.log("Login Correcto");
+        }else{
+            console.log("Credenciales invalidas");
+        }
+    }
+    const handleSummit = async (e) => {
+        e.preventDefault();
+        await tokenAuth({ variables: { email: datalogin.email, password: datalogin.password } });
+        setDatalogin({ email: '', password: '' });
+    }
+    
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col lg={4} md={6} sm={12} className="pt m-auto shadow-sm rounded-lg">
+                        <Form className="form-border"
+                            
+                            >
+                            <Row className="icon-user">
+                                <FaUserCircle color={COLORS.carnelian} size={150} />
+                            </Row>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <InputGroup>
+                                    <InputGroup.Text className="icon-login">
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <InputGroup>
-                  <InputGroup.Text>
-                    <RiLockPasswordFill color={COLORS.carnelian} size={30} />
-                  </InputGroup.Text>
+                                        <FaUserCircle color="black" size={30} />
+                                    </InputGroup.Text>
+                                    <Form.Control
+                                        onChange={(e) =>
+                                            
+                                            {
+                                                e.preventDefault();
+                                                setDatalogin({
+                                                ...datalogin,
+                                                email: e.target.value
+                                            })}}
+                                        type="email"
+                                        placeholder="Ingrese su correo electronico"
+                                    />
+                                </InputGroup>
+                            </Form.Group>
 
-                  <Form.Control
-                    type="password"
-                    placeholder="Ingrese su contraseña"
-                  />
-                </InputGroup>
-              </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <InputGroup>
+                                    <InputGroup.Text className="icon-login">
+                                        <RiLockPasswordFill color="black" size={30} />
+                                    </InputGroup.Text>
 
-              <Row className="p-2" xs={1} sm={1} lg={2}>
-                <Col className="p-2">
-                  <Button
-                    className="button-login"
-                    variant="primary"
-                    type="submit"
-                  >
-                    Iniciar Sesion
-                  </Button>
-                </Col>
-                <Col className="p-2">
-                  <Button className="button-login" variant="success">
-                    Registrarse
-                  </Button>
-                </Col>
-              </Row>
-              <Button className="button-login" variant="primary" type="submit">
-                <AiFillGoogleCircle size={40} />
-                &ensp; Iniciar Sesion con Google
-              </Button>
-              <Button className="button-login" variant="link">
-                Olvide mi contraseña
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+                                    <Form.Control
+                                        onChange={(e) =>
+                                            setDatalogin({
+                                                ...datalogin,
+                                                password: e.target.value
+                                            })}
+                                        type="password"
+                                        placeholder="Ingrese su contraseña"
+                                    />
+                                </InputGroup>
+                            </Form.Group>
+
+                            <Row className="p-2" xs={1} sm={1} lg={2}>
+                                <Col className="p-2">
+                                    <Button
+                                        className="button-login"
+                                        id="login"
+                                        variant="outline-primary"
+                                        type="submit"
+                                        onChange = {handleSummit}
+                                    >
+                                        Iniciar Sesion
+                                    </Button>
+                                </Col>
+                                <Col className="p-2">
+                                    <Button
+                                        className="button-login-r"
+                                        variant="success"
+                                    >
+                                        Registrarse
+                                    </Button>
+                                </Col>
+                            </Row>
+                            <Button className="button-login-google" variant={COLORS.lightGray} type="submit">
+                                <AiFillGoogleCircle size={40} />
+                                &ensp; Iniciar Sesion con Google
+                            </Button>
+                            <Button className="button-forgot-password" variant="link">
+                                Olvide mi contraseña
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+                
+            </Container>
+        </>
+    );
 };
---
+++ src/graphql/users/LOGIN.js
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+const LOGIN = gql`
+mutation login($email: String!, $password: String!){
+    tokenAuth(email: $email, password: $password) {
+        success,
+        errors,
+        unarchiving,
+        token,
+        #refreshToken,
+        unarchiving,
+        user {
+            id,
+            firstName
+            email,
+            verified
+        }
+    }
+}`;
+
+export default LOGIN;
