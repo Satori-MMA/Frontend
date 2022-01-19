b76986ebad7f59de839e7df5a8fd2db8ae5072d5
Oscar Chauza <ochauza@unicauca.edu.co>
2022-01-19 12:43:56 -0500

    login funcionando

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -11,9 +11,6 @@ import { Navigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const infoLogin = () => {
-
-} 
 
 export const Login = () => {
   const [tokenAuth, { data, loading, error, reset }] = useMutation(LOGIN);
