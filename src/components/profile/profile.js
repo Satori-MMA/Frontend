6dd67cc6648f7a192f3c0bf79936349b7c891365
David Sotelo <juandavid.sotelo.m@gmail.com>
2022-01-26 16:22:13 -0500

    Card de usuario para el perfil

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,10 +1,17 @@
+import { Row, Col} from "react-bootstrap";
 import { LoadingMessage } from "../utilities/LoadingMessage";
+import { UserCard } from "./userCard";
 
 export const Profile = () => {
   return (
     <div>      
       <h1 className="text-white text-center"> Perfil </h1>    
       <LoadingMessage/>
+      <Row>
+        <Col>
+          <UserCard />
+        </Col>
+      </Row>
     </div>    
   );
 };
