672bfa8c1ec3ab7999d9130b2871c498286d7dbf
David Sotelo <juandavid.sotelo.m@gmail.com>
2022-01-31 15:37:42 -0500

    Imagen provisional en la vista de cursos

--- src/components/courses/courseCard.js
+++ src/components/courses/courseCard.js
@@ -1,25 +1,33 @@
-import {Card,Button} from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-const CourseCard = ({course,link,name}) =>{
-    return(
-        <Card key={course.id} bg="dark" >
-        <Card.Img variant="top" src="" title="imagen" />
-        <Card.Body>
-            <Card.Title>{course.coTitle}</Card.Title>
-            <Card.Text>
-                {course.coDescription}
-            </Card.Text>
-            <hr/>
-            <p>Precio <span className="text-danger">$</span>{course.coPrice}</p>
-            <Button
-            className="button-login-r mb-1"
-            as={Link}
-            to={{pathname: `/${link}/${course.coTitle}`}}
-            variant="primary"
-            >{name}</Button>
-        </Card.Body>
+import { ReactComponent as FightSVG } from "../../Assets/fight.svg";
+import { COLORS } from "../utilities/color";
+const CourseCard = ({ course, link, name }) => {
+  return (
+    <Card key={course.id} bg="dark">
+      <center>
+        <FightSVG fill={COLORS.rubyRed} width="70%" height="auto"/>
+      </center>
+      {/* <Card.Img variant="top" src="" title="imagen" /> */}
+      <Card.Body>
+        <Card.Title>{course.coTitle}</Card.Title>
+        <Card.Text>{course.coDescription}</Card.Text>
+        <hr />
+        <p>
+          Precio <span className="text-danger">$</span>
+          {course.coPrice}
+        </p>
+        <Button
+          className="button-login-r mb-1"
+          as={Link}
+          to={{ pathname: `/${link}/${course.coTitle}` }}
+          variant="primary"
+        >
+          {name}
+        </Button>
+      </Card.Body>
     </Card>
-    );
-}
+  );
+};
 
 export default CourseCard;
