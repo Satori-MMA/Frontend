3b6adf1a40284c33ba73109df1935aa8fcaf93d9
AndresPinzon14 <andrespinzon1414@gmail.com>
2022-03-14 15:59:03 -0500

    logo estudiante y profesor

--- src/components/profile/userCard.js
+++ src/components/profile/userCard.js
@@ -3,49 +3,61 @@ import { FcBusinessman } from "react-icons/fc";
 import { useGlobalState } from "../GlobalState";
 import { Link } from "react-router-dom";
 import { COLORS } from "../utilities/color";
-import Logo from "../../Assets/Logo2.png";
-export const UserCard = ({user}) => {
+import CoachLogo from "../../Assets/CoachLogo.png";
+import StudentLogo from "../../Assets/StudentLogo.png";
+export const UserCard = ({ user }) => {
   return (
     <Card className="no-border-card">
       <Card.Body className="bg-ourBlack form-border m-0">
         <Row>
           <div>
-          <img
-          className="mt-3 mb-3"
-          alt="Centro de entrenamiento Satori - Horario"
-          width="40%"
-          height="auto"
-          src={Logo}
-        />
+            {user?.rolUser?.edges[0]?.node.rolName === "TEACHER" ? (
+              <img
+                className="mt-3 mb-3"
+                alt="Centro de entrenamiento Satori - Horario"
+                width="85%"
+                height="auto"
+                src={CoachLogo}
+              />
+            ) : (
+              <img
+                className="mt-3 mb-3"
+                alt="Centro de entrenamiento Satori - Horario"
+                width="85%"
+                height="auto"
+                src={StudentLogo}
+              />
+            )}           
           </div>
 
-          <Card.Title>
-            {user?.rolUser?.edges[0]?.node.rolName} {user.firstName}
+          <Card.Title className="text-carnelian">
+            {user.firstName}
           </Card.Title>
           <Card.Text className="p-2">
             <p>
-              <strong>Nombre completo: </strong> {user.firstName}{" "}
+              <strong className="text-imperialRed">Nombre completo: </strong> {user.firstName}{" "}
               {user.lastName}
               <br />
-              <strong>email: </strong>
+              <strong className="text-imperialRed">email: </strong>
               {user.email}
               <br />
-              <strong>Número de teléfono: </strong>
+              <strong className="text-imperialRed">Número de teléfono: </strong>
               {user.userPhone}
               <br />
-              <strong>Dirección: </strong>
+              <strong className="text-imperialRed">Dirección: </strong>
               {user.userAddress}
               <br />
             </p>
           </Card.Text>
         </Row>
         <Button
-            className="button-login-r bottom mb-1"
-            as={Link} to="/profileupdate"
-            variant="primary"
-          >
-            Actualizar Informacion
-          </Button>
+          className="button-login-r bottom mb-1"
+          as={Link}
+          to="/profileupdate"
+          variant="primary"
+        >
+          Actualizar Informacion
+        </Button>
       </Card.Body>
     </Card>
   );
